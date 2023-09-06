48e31daca11f1b9d3c7f0294d16007d497b659b3
Forrest Milner <fmilner@zoom456.com>
2023-09-06 14:10:13 -0400

    script not available until cloned... duh.

--- installdependencies.sh
+++ installdependencies.sh
@@ -2,8 +2,6 @@
 sudo apt-get -y install v4l-utils
 sudo apt install python3
 sudo apt install python3-venv
-sudo git clone https://github.com/umdenes100/Vision-System-Python.git
-sudo cd Vision-System-Python
 sudo python3 -m venv .venv
 sudo source .venv/bin/activate
 sudo pip3 install -r requirements.txt
